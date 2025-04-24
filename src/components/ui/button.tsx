eb40e06bc0957d87cc35defd57e67917291fdce6
felduque557 <jf.duque557@gmail.com>
2025-04-24 16:11:34 -0500

    feat(ui): añadir componentes de tarjeta e insignia y refactorizar estilos de botón
    Añadir nuevos componentes de tarjeta e insignia a la biblioteca de interfaz de usuario, proporcionando elementos reutilizables y personalizables. Refactorización del orden de las clases CSS del componente Button para mejorar la legibilidad y la facilidad de mantenimiento.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex justify-center items-center gap-2 disabled:opacity-50 rounded-md focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring [&_svg]:size-4 font-medium text-sm whitespace-nowrap transition-colors disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0",
   {
     variants: {
       variant: {
